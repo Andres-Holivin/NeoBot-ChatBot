9855014baeee38c62ea49a01e329939a861b793c
andres holivin <30800318+holivin21@users.noreply.github.com>
2022-01-24 00:06:49 +0700

    home screen

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,7 +5,7 @@ import { Color } from './Variable';
 const Home = () => {
   return(
     <Container justifyContent="center" alignItems="center" p="10px" display="flex" m="5%" bgColor={Color.Secondary} w="100%" borderRadius="20px">
-        <Grid templateColumns="40% 60%" w="100%">
+        <Grid templateColumns="40% 60%" w="100%" alignItems="center">
             <Image src='https://res.cloudinary.com/holivin21/image/upload/v1642957490/NeoBot/Pngtree_artificial_intelligence_mobile_voice_assistant_6447013_sbghg3.png' boxSize="300px"/>
             <Heading as="h1">NeoBot</Heading>
         </Grid>
